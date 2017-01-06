8981b9668aae4d987b34ed4083893d04edb59774
Louis DEKEISTER <louis@hopstudios.com>
2017-01-06 10:47:22 -0800

    bump to v 0.0.2

--- ee3x/system/user/addons/hop_alerts_fetch/config.php
+++ ee3x/system/user/addons/hop_alerts_fetch/config.php
@@ -9,11 +9,11 @@
  * @author		Hop Studios, Inc.
  * @copyright	Copyright (c) 2014, Hop Studios, Inc.
  * @link		http://www.hopstudios.com/software/
- * @version		0.0.1
+ * @version		0.0.2
  */
 
 $config['name']='Hop Alerts Fetch';
-$config['version']='0.0.1';
+$config['version']='0.0.2';
 // $config['nsm_addon_updater']['versions_xml']='http://www.hopstudios.com/software/versions/';
 
 // Version constant
