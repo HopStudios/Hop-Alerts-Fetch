6a5cbd76bf6b64d0b369063b1290a286bb7c64c2
Louis DEKEISTER <louis@hopstudios.com>
2017-01-09 12:18:11 -0800

    add info on add-on main page about alert sources

--- ee3x/system/user/addons/hop_alerts_fetch/views/index.php
+++ ee3x/system/user/addons/hop_alerts_fetch/views/index.php
@@ -1,5 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<div class="box">
-	<p>&nbsp; &nbsp; &nbsp; &nbsp; Click on Settings</p>
+<div class="box" style="padding-left: 35px;">
+	<p>Click on Settings</p>
+	<p>The add-on fetches alerts from:</p>
+	<ul>
+		<li>MARC Trains (<a href="http://mtamarylandalerts.com/rss.aspx?ma">link</a>)</li>
+		<li>VRE Trains (<a href="https://public.govdelivery.com/accounts/VAVRE/feed.rss">link</a>)</li>
+		<li>ART Bus (<a href="http://www.commuterpage.com/RSS/artalert_rss.xml">link</a>)</li>
+		<li>Montgomery RideOn Bus (<a href="http://www.montgomerycountymd.gov/dot-rss/resources/files/rss/rideonrss.xml">link</a>)</li>
+		<li>Washington Metropolitan Area Rail &amp; Bus (<a href="https://developer.wmata.com/">link</a>)</li>
+		<li>Washington Top News Traffic (<a href="https://twitter.com/WTOPtraffic">link</a>)</li>
+	</ul>
 </div>
