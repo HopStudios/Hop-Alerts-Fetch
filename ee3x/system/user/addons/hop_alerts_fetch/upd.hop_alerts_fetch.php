af99fce63992136f37b3a7d621be8b100e50021b
Louis DEKEISTER <louis@hopstudios.com>
2017-01-09 12:10:50 -0800

    bump to v 0.0.3

--- ee3x/system/user/addons/hop_alerts_fetch/upd.hop_alerts_fetch.php
+++ ee3x/system/user/addons/hop_alerts_fetch/upd.hop_alerts_fetch.php
@@ -64,7 +64,7 @@ class Hop_alerts_fetch_upd {
 	{
 		ee()->load->dbforge();
 
-		if (version_compare($current, '0.0.2', '='))
+		if (version_compare($current, '0.0.3', '='))
 		{
 			return FALSE;
 		}
